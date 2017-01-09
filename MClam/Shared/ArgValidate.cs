1ea8ba63831b144bf96e92901d53094f80f901bd
fahminlb33 <fahminlb33@gmail.com>
2017-01-09 17:46:07 +0700

    contracts replacement

---
+++ MClam/Shared/ArgValidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MClam.Shared
+{
+    internal static class ArgValidate
+    {
+        public static void NotNull(object obj, string paramName)
+        {
+            if (obj == null) throw new ArgumentNullException(paramName);
+        }
+
+        public static void NotEmptyString(string s, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(s)) throw new ArgumentNullException(paramName);
+        }
+
+        public static void FileExist(string path, string paramName)
+        {
+            if (!File.Exists(path)) throw new ArgumentException("File not found!", paramName);
+        }
+    }
+}
