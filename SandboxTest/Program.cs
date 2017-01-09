22eaa5d20d252e40db2b8ab026c5a3cb6ed52413
fahminlb33 <fahminlb33@gmail.com>
2017-01-09 17:57:49 +0700

    [skip ci]

--- SandboxTest/Program.cs
+++ SandboxTest/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using MClam;
 
 namespace SandboxTest
 {
@@ -15,28 +16,24 @@ namespace SandboxTest
             var st = new Stopwatch();
             st.Start();
 
-            var crypt = SHA1.Create();
-            using (var file = new FileStream("D:\\bin\\clamav\\libclamav.dll", FileMode.Open))
-            {
-                var hash = crypt.ComputeHash(file);
-                Console.WriteLine(ArrayToHex(hash));
-                Console.WriteLine(hash.Length);
-                Console.WriteLine(file.Length);
-            }
+            Console.WriteLine("Capturing...");
+            ClamMain.Initialize();
+
+            var dataChecker = new DatabaseStateChecker(@"D:\bin\database\test");
+            dataChecker.CaptureState();
+
+            Console.WriteLine("Do changes.");
+            Console.ReadKey();
+
+            Console.WriteLine("Changes detected: " + dataChecker.HasChanges());
+
+            Console.WriteLine("Release.");
+            dataChecker.ReleaseCapture();
 
             st.Stop();
             Console.WriteLine("Elapsed: " + st.ElapsedMilliseconds);
             Console.ReadKey();
         }
 
-        private static string ArrayToHex(byte[] input)
-        {
-            var sb = new StringBuilder();
-            for (int i = 0; i < input.Length; i++)
-            {
-                sb.Append(input[i].ToString("x2"));
-            }
-            return sb.ToString();
-        }
     }
 }
