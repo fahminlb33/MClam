a5664d4233ad91344b72dd2b170a58e1c8d5000e
fahminlb33 <fahminlb33@gmail.com>
2017-01-09 17:59:55 +0700

    optimized

--- MClam/Shared/ArgValidate.cs
+++ MClam/Shared/ArgValidate.cs
@@ -1,29 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace MClam.Shared
 {
     internal static class ArgValidate
     {
-        public static void NotNull(object obj, string paramName)
+        internal static void NotNull(object obj, string paramName)
         {
             if (obj == null) throw new ArgumentNullException(paramName);
         }
 
-        public static void NotEmptyString(string s, string paramName)
+        internal static void NotEmptyString(string s, string paramName)
         {
             if (string.IsNullOrWhiteSpace(s)) throw new ArgumentNullException(paramName);
         }
 
-        public static void FileExist(string path, string paramName)
+        internal static void FileExist(string path, string paramName)
         {
             if (!File.Exists(path)) throw new ArgumentException("File not found!", paramName);
         }
 
-        public static void DirectoryExist(string path, string paramName)
+        internal static void DirectoryExist(string path, string paramName)
         {
             if (!Directory.Exists(path)) throw new ArgumentException("Directory not found!", paramName);
         }
